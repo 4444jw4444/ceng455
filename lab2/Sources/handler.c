1e31372d17d701cca615ec2df8a862dc9f7b2a36
Stephen Bos <sjlbos@outlook.com>
2016-02-20 12:15:55 -0800

    Handler skeleton in place
    - Added handler types and interface declarations
    - Initial implementation of OpenW and PutLine

--- lab2/Sources/handler.c
+++ lab2/Sources/handler.c
@@ -1,3 +1,83 @@
 #include "handler.h"
 
+/*=============================================================
+                      BUFFER MANAGEMENT
+ ==============================================================*/
+
+/*=============================================================
+                      READER MANAGEMENT
+ ==============================================================*/
+
+/*=============================================================
+                      WRITER MANAGEMENT
+ ==============================================================*/
+
+void _clearHandlerWriter(){
+
+}
+
+/*=============================================================
+                      USER TASK INTERFACE
+ ==============================================================*/
+
+bool OpenR(uint16_t streamNumber){
+	return false;
+}
+
+bool GetLine(char* outputString){
+	return false;
+}
+
+_queue_id OpenW(void){
+	if(_mutex_lock(&g_Handler->accessMutex) != MQX_OK){
+		printf("Mutex lock failed.\n");
+		_task_block();
+	}
+
+	_task_id writer = g_Handler->currentWriter;
+
+	if (writer != 0){
+		_mutex_unlock(&g_Handler->accessMutex);
+		return 0;
+	}
+
+	g_Handler->currentWriter = _task_get_id();
+	_queue_id outputQueue = g_Handler->bufferOutputQueue;
+
+	_mutex_unlock(&g_Handler->accessMutex);
+	return outputQueue;
+}
+
+bool PutLine(_queue_id queueId, char* inputString){
+
+	// Ensure last char is a newline
+	int stringLen = strlen(inputString);
+	if (inputString[stringLen - 1] != '\n'){
+		return false;
+	}
+
+	// Allocate and initialize serial message
+	SerialMessagePtr writeMessage = (SerialMessagePtr)_msg_alloc(g_SerialMessagePool);
+	if (writeMessage == NULL) {
+	 printf("Could not allocate a message.\n");
+	 _task_block();
+	}
+
+	writeMessage->HEADER.SIZE = sizeof(SerialMessage);
+	writeMessage->HEADER.TARGET_QID = queueId;
+	writeMessage->length = stringLen;
+	writeMessage->message = inputString;
+
+	// Write serial message to queue
+	if (!_msgq_send(writeMessage)) {
+	 printf("Could not send a message.\n");
+	 _task_block();
+	}
+
+	return true;
+}
+
+bool Close(void){
+	return false;
+}
 
--- lab2/Sources/handler.h
+++ lab2/Sources/handler.h
@@ -1,12 +1,72 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <mqx.h>
+#include <message.h>
+#include <mutex.h>
+
 #ifndef SOURCES_HANDLER_H_
 #define SOURCES_HANDLER_H_
 
-#include <stdbool.h>
-/*
-bool OpenR(uint_16 streamNumber);
+/*=============================================================
+                         CONSTANTS
+ ==============================================================*/
+
+#define SERIAL_MESSAGE_POOL_INITIAL_SIZE 16
+#define SERIAL_MESSAGE_POOL_GROWTH_RATE 16
+#define SERIAL_MESSAGE_POOL_MAX_SIZE 2048
+
+/*=============================================================
+                      EXPORTED TYPES
+ ==============================================================*/
+
+typedef struct HandlerBuffer {
+	int currentSize;
+	int maxSize;
+	char* buffer;
+} HandlerBuffer, * HandlerBufferPtr;
+
+typedef struct HandlerReader{
+	_task_id taskId;
+	_queue_id queueId;
+} HandlerReader, *HandlerReaderPtr;
+
+typedef struct HandlerReaderList{
+	int count;
+	int maxSize;
+	HandlerReaderPtr readers;
+} HandlerReaderList;
+
+typedef struct Handler{
+	HandlerReaderList readerList;
+	HandlerBuffer buffer;
+	_task_id currentWriter;
+	MUTEX_STRUCT accessMutex;
+	MUTEX_STRUCT readerMutex;
+	_queue_id charInputQueue;
+	_queue_id bufferOutputQueue;
+} Handler, * HandlerPtr;
+
+typedef struct SerialMessage{
+	MESSAGE_HEADER_STRUCT HEADER;
+	int length;
+	char* message;
+} SerialMessage, * SerialMessagePtr;
+
+/*=============================================================
+                      GLOBAL VARIABLES
+ ==============================================================*/
+
+extern HandlerPtr g_Handler;
+extern _pool_id g_SerialMessagePool;
+
+/*=============================================================
+                      USER TASK INTERFACE
+ ==============================================================*/
+
+bool OpenR(uint16_t streamNumber);
 bool GetLine(char* outputString);
 _queue_id OpenW(void);
 bool PutLine(_queue_id queueId, char* inputString);
 bool Close(void);
-*/
+
 #endif
